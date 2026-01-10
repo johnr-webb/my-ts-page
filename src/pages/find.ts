9210f054da47dcf3d6319893ff5257da707109bd
John Webb <johnwebb354@gmail.com>
2026-01-09 19:44:32 -0600

    Adding message to find page

--- src/pages/find.ts
+++ src/pages/find.ts
@@ -1,12 +1,14 @@
 export function renderFindPage(appElement: HTMLElement) {
   const content = `
     <h1>Find Page</h1>
-    <p>This is the Find page where you can search for locations.</p>
-    <div>
-      <h3>Search for a Location</h3>
-      <input type="text" placeholder="Enter location..." />
-      <button id="search-btn">Search</button>
-    </div>
+
+    <p>
+      🚧 This page is currently in the works.
+    </p>
+
+    <p>
+      I'm working on it! Check back in a bit!
+    </p>
   `;
 
   appElement.innerHTML = content;
